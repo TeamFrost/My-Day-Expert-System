1faa286667864287bfac1625de6396e9d28e0ce8
Simonesiss <simona.radulea@gmail.com>
2021-03-19 17:48:53 +0200

    Small update

--- expert-system/src/App.js
+++ expert-system/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={console.log("smth")}>Start</button>
         </p>
         <a
           className="App-link"
